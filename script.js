2d5e77137b4bfc499530881a0e6b019ed8a905ed
Aman Kumar <125535132+Aman13052002@users.noreply.github.com>
2026-05-01 12:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const submitBtn = document.getElementById('submit');
+const booklist = document.getElementById('book-list');
+
+
+submitBtn.addEventListener('click', function(){
+	const title = document.getElementById('title').value.trim();
+	const author = document.getElementById('author').value.trim();
+	const isbn = document.getElementById('isbn').value.trim();
+
+	const newRow = document.createElement('tr');
+
+	newRow.innerHTML= `
+	<td>${title}</td>
+	<td>${author}</td>
+	<td>${isbn}</td>
+	<td>
+	<button class='delete'>Clear</button>
+	</td>	
+	`
+
+	booklist.appendChild(newRow);
+
+	newRow.querySelector('.delete')
+		.addEventListener('click', ()=> newRow.remove())
+
+});
